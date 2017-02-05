8d7a7890208657f031b8de7d520e80f15012b271
suryadevops <suryadevops98@gmail.com>
2017-02-05 12:30:26 -0500

    java file from eclipse

---
+++ myfirstgit/printinghello.java
@@ -0,0 +1,12 @@
+package myfirstgit;
+
+public class printinghello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("***************************");
+		System.out.println("*	GIT           	*");
+		System.out.println("***************************");
+	}
+
+}
